c2378a956f4539073940fb08ff61c35484e0a80a
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2022-06-26 07:43:04 -0300

    Primeiro commit do repositório...

---
+++ app/controllers/NoticiaController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class NoticiaController extends ControllerBase
+{
+    private $mensagemDeErro = '';
+
+    public function listaAction()
+    {
+
+        $this->view->pick("noticia/listar");
+    }
+
+    public function cadastrarAction()
+    {
+        
+        $this->view->pick("noticia/cadastrar");
+
+    }
+
+    public function editarAction($id)
+    {
+        $this->view->pick("noticia/editar");
+    }
+
+    public function salvarAction()
+    {
+        return $this->response->redirect(array('for' => 'noticia.lista'));
+    }
+
+     public function excluirAction($id)
+     {
+        return $this->response->redirect(array('for' => 'noticia.lista'));
+     }
+
+}
